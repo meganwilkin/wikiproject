9d0f094b155a0f716c8e0bceaeb1d21cfffc72c9
Megan Wilkin <megan.wilkin@blueostrichdesign.co.uk>
2017-05-29 14:22:23 +0100

    standard user able to CRUD

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :wikis
+
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable,
          :authentication_keys => [:login]
---
+++ spec/controllers/wikis_controller_spec.rb
@@ -0,0 +1,71 @@
+require 'rails_helper'
+
+RSpec.describe WikisController, type: :controller do
+
+  # before do
+  #       user = User.create!(name: "Wiki User", email: "user@wiki.com", password: "helloworld")
+  #       create_session(user)
+  #     end
+# let(:my_wiki) { Wiki.create!(title: 'New Wiki Title', body: 'Body of New Wiki Title', private: false, user_id: nil)}
+
+  describe "GET #index" do
+    it "returns http success" do
+      get :index
+      expect(response).to redirect_to new_user_session_path
+    end
+    #
+    # it "assigns [my_wiki] to @wikis" do
+    #    get :index
+    #    expect(assigns(:wikis)).to eq([my_wiki])
+    #  end
+  end
+
+  # describe "GET new" do
+  #
+  #     it "returns http success" do
+  #       get :new
+  #       expect(response).to have_http_status(:success)
+  #     end
+  #
+  #     it "renders the #new view" do
+  #       get :new
+  #       expect(response).to render_template :new
+  #     end
+  #
+  #     it "instantiates @pwiki" do
+  #       get :new
+  #       expect(assigns(:wiki)).not_to be_nil
+  #     end
+  #   end
+  #
+  #   describe "WIKI create" do
+  #     it "increases the number of Wiki by 1" do
+  #       expect{wiki :create, wiki: {title: "My exciting Wiki", body: "The body of my exciting wiki", private: false, user_id: nil}}.to change(Wiki,:count).by(1)
+  #     end
+  #
+  #     it "assigns the new wilw to @wiki" do
+  #       wiki :create, wiki: {title: "My exciting Wiki", body: "The body of my exciting wiki", private: false, user_id: nil}
+  #       expect(assigns(:wiki)).to eq Wiki.last
+  #     end
+  #
+  #     it "redirects to the new wiki" do
+  #       wiki :create, wiki: {title: "My exciting Wiki", body: "The body of my exciting wiki", private: false, user_id: nil}
+  #       expect(response).to redirect_to Wiki.last
+  #     end
+  #   end
+end
+
+  # describe "GET #show" do
+  #   it "returns http success" do
+  #     get :show
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
+  #
+
+  # describe "GET #edit" do
+  #   it "returns http success" do
+  #     get :edit
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
